bd268bba5b0a9549c8854d5b9bc28a6c104b7bc8
Zia-Mahmood <ziamh2003@gmail.com>
2025-04-15 13:20:44 +0530

    Added root Layout for Frontend and beautified Login

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -1,41 +1,31 @@
 import "../styles/Login.css";
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Box, Button, Container, Grid, IconButton, InputAdornment, TextField, Typography, Paper } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import axios from 'axios';
+
+const url = "http://localhost:3000/api";
 
 // functional component
 function Login({ onLogin }) {
-    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState('');
 
     const navigate = useNavigate();
 
     useEffect(() => {
-        // Check if the user is already logged in
-
-        // axios.get('http://localhost:5001/api/isAuth', { withCredentials: true })
-        //     .then(response => {
-        //         // If user is authenticated, redirect to dashboard or homepage
-        //         if (response.status === 200) {
-        //             navigate('/home'); // redirect to your protected page
-        //         }
-        //     })
-        //     .catch(error => {
-        //         console.log('User is not authenticated');
-        //     });
         const checkAuth = async () => {
-            //e.preventDefault();
             try {
-                const response = await fetch('http://localhost:5001/api/isAuth', {
+                const response = await fetch(url + '/isAuth', {
                     method: 'GET',
+                    withCredentials: true,
                     credentials: 'include'
                 });
                 if (response.ok) {
-                    //setError('');
-                    //alert("Login successful!");
-                    //onLogin();
-                    navigate('/home');
+                    navigate('/');
                 } else {
                     const errorData = await response.text(); // Get the raw text to see the error
                     console.log('User is not authenticated:', errorData);
@@ -50,19 +40,19 @@ function Login({ onLogin }) {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        if (!username || !password) {
+        if (!email || !password) {
             setError('Please enter both username and password');
             return;
         }
         try {
-            const response = await fetch('http://localhost:5001/api/login', {
+            const response = await fetch(url + '/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 credentials: 'include',
                 body: JSON.stringify({
-                    username,
+                    email,
                     password,
                 }),
             });
@@ -71,9 +61,10 @@ function Login({ onLogin }) {
 
             if (response.ok) {
                 setError('');
-                alert("Login successful!");
+                console.log(data)
+                //alert("Login successful!");
                 onLogin();
-                navigate('/home');
+                navigate('/');
             } else {
                 setError(data.msg || 'Login failed');
             }
@@ -88,34 +79,112 @@ function Login({ onLogin }) {
     };
 
     return (
-        <div className="login-container">
-            <h2>Login</h2>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label>Username:</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        placeholder="Enter username"
+        <Box sx={{display:'flex', flexDirection:'column',mt:'5%'}}>
+            <Container maxWidth="sm" sx={{ alignSelf: 'center', flexDirection: 'column',width:'40%' }}>
+                <Box component="img" sx={{width:'100%',height:'auto',mb:8}} src="/iiit-logo-white.png" alt="IIITH Logo" />
+                <Typography variant="h5" sx={{ mb: 3, color: '#FFFFFF', fontWeight: 600 ,textAlign:'center'}}>
+                    Enter Credentials
+                </Typography>
+                <Box component="form" onSubmit={handleLogin} noValidate>
+                    <TextField
+                        fullWidth
+                        required
+                        placeholder="Email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        margin="normal"
+                        variant="standard"
+                        InputLabelProps={{ style: { color: '#FFFFFF',background:'transparent' } }}
+                        InputProps={{
+                            style: { color: '#FFFFFF' },
+                            sx: {
+                                '& .MuiOutlinedInput-notchedOutline': { borderColor: '#ffffff' },
+                                '&:hover .MuiOutlinedInput-notchedOutline': { borderColor: '#8EB8FF' },
+                                '&.Mui-focused .MuiOutlinedInput-notchedOutline': { borderColor: '#8EB8FF' },
+                            },
+                        }}
                     />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
+
+                    <TextField
+                        fullWidth
+                        required
+                        type={showPassword ? 'text' : 'password'}
                         value={password}
+                        placeholder="Password"
                         onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Enter password"
+                        margin="normal"
+                        variant="standard"
+                        InputLabelProps={{ style: { color: '#A3A8C5' } }}
+                        InputProps={{
+                            style: { color: '#FFFFFF' },
+                            sx: {
+                                '& .MuiOutlinedInput-notchedOutline': { borderColor: '#A3A8C5' },
+                                '&:hover .MuiOutlinedInput-notchedOutline': { borderColor: '#A3A8C5' },
+                                '&.Mui-focused .MuiOutlinedInput-notchedOutline': { borderColor: '#A3A8C5' },
+                            },
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        edge="end"
+                                        sx={{ color: '#A3A8C5' }}
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
                     />
-                </div>
-                <div class="button-container">
-                    <button type="submit">Login</button>
-                    <button type="button" onClick={handleSignUp}>Sign Up</button>
-                </div>
-            </form>
-        </div>
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{
+                            mt: 4,
+                            py: 1.5,
+                            borderRadius: '999px',
+                            backgroundColor: '#7A7A7A',
+                            fontWeight: 700,
+                            fontSize: '1.125rem',
+                            '&:hover': { backgroundColor: '#548eb8' },
+                        }}
+                    >
+                        Log In
+                    </Button>
+                </Box>
+            </Container>
+        </Box>
+
+        // <div className="login-container">
+        //     <div className="logo-div">
+        //         <img src="/iiit-logo-white.png" alt="IIITH-LOGO"/>
+        //     </div>
+        //     <h2>Enter Credentials</h2>
+        //     {error && <p style={{ color: 'red' }}>{error}</p>}
+        //     <form onSubmit={handleLogin}>
+        //         <div>
+        //             <input
+        //                 type="text"
+        //                 value={email}
+        //                 onChange={(e) => setEmail(e.target.value)}
+        //                 placeholder="Email"
+        //             />
+        //         </div>
+        //         <div>
+        //             <input
+        //                 type="password"
+        //                 value={password}
+        //                 onChange={(e) => setPassword(e.target.value)}
+        //                 placeholder="Password"
+        //             />
+        //         </div>
+        //         <div class="button-container">
+        //             <button type="submit">Login</button>
+        //         </div>
+        //     </form>
+        // </div>
     );
 }
 
